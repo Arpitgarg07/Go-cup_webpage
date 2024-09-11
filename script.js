551162dd79f44a6a31735a44edac4f40fc166bd1
Arpit garg <arpitgarg5689@gmail.com>
2024-09-12 00:01:12 +0530

    mobile version update

--- script.js
+++ script.js
@@ -13,6 +13,17 @@ document.addEventListener("DOMContentLoaded", function() {
     var iframe = document.createElement('iframe');
     iframe.src = "slideshow-aboutus/index.html";
     iframe.style.width = "100%";
+    iframe.style.height = "auto"; // Auto-adjust height based on content
+    iframe.style.border = "0";
+    iframe.style.minHeight = "500px"; // Set a minimum height if needed
+    container.appendChild(iframe);
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var container = document.getElementById("ad-machine");
+    var iframe = document.createElement('iframe');
+    iframe.src = "coffeemachine/index.html";
+    iframe.style.width = "100%";
     iframe.style.height = "100vh";
     iframe.style.border = "0";
     container.appendChild(iframe);
